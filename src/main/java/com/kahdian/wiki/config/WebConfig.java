2502d362a8095bbd88ee94ec7d1bef48332f607c
David Kahdian <dkahdian@g.ucla.edu>
2025-09-01 08:27:54 -0700

    Fixed CORS bug

---
+++ src/main/java/com/kahdian/wiki/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.kahdian.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
